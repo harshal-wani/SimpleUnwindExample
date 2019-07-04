178b8e6162a074a99cefe29abbd9457657511aa2
Harshal <harshal.wani@thinkpower-info.com>
2019-07-04 12:08:17 +0800

    Add example project

---
+++ SimpleUnwindExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SimpleUnwindExample
+//
+//  Created by Harshal Wani on 04/07/19.
+//  Copyright © 2019 Harshal Wani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //Use unwindToCancel method navigate back to this screen.
+    
+    @IBAction func unwindToCancel(_ unwindSegue: UIStoryboardSegue) {
+        // Use data from the view controller which initiated the unwind segue
+    }
+}
+
